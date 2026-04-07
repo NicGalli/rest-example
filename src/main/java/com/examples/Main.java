ee1666a01ee924403d008777b0f83a2a56f10239
NicGalli <nikk094@live.it>
2026-04-07 11:58:14 +0200

    heroku preparation

--- src/main/java/com/examples/Main.java
+++ src/main/java/com/examples/Main.java
@@ -26,7 +26,8 @@ import jakarta.inject.Singleton;
 public class Main {
 
 	// Base URI the Grizzly HTTP server will listen on
-	public static final String BASE_URI = "http://localhost:8080/";
+	public static final String BASE_URI = "http://0.0.0.0:"
+		+ System.getProperty("simple.rest.service.port", "8080") + "/myapp/";
 
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
